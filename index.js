53dae835d4de7efdf3aad86c8a32a4a5c4b191ae
loren.grixti <lorengrx@gmail.com>
2017-09-02 15:13:22 -0400

    adding CORS support on server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+//Middleware: Allows cross-domain requests (CORS)
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 var twitterProxyServer = require('twitter-proxy');
 var twitterAPIKeys = require('./config.json');
 twitterProxyServer(twitterAPIKeys);
@@ -7,7 +16,7 @@ twitterProxyServer(twitterAPIKeys);
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
     response.render('public/index');
